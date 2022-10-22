8cc4f3cdc057973f4678a67e6845d3265f0dd43f
Jin <admin@at.ci>
2022-10-23 00:57:53 +0800

    control set get

--- key-value/src/main/java/ke/bb/Control.kt
+++ key-value/src/main/java/ke/bb/Control.kt
@@ -6,8 +6,29 @@ import android.database.sqlite.SQLiteDatabase
 
 class Control(context: Context) {
     data class KeyValueItem(val group: String, val key: String, val value: String, val updatedAt: Long)
+
     private val keyValueDbHelper = KeyValueDbHelper(context)
     private val db = keyValueDbHelper.writableDatabase
+
+    private val properties: MutableMap<String, KeyValueItem> =
+        all().associateBy { it.group + "_" + it.key }.toMutableMap()
+
+    fun get(group: String, key: String): String? {
+        return properties[group + "_" + key]?.value
+    }
+
+    fun set(group: String, key: String, value: String): Boolean {
+        return upsert(group, key, value).apply {
+            if (this) {
+                properties[group + "_" + key] = KeyValueItem(group, key, value, System.currentTimeMillis())
+            }
+        }
+    }
+
+    fun getRaw(): MutableMap<String, KeyValueItem> {
+        return properties
+    }
+
     private fun all(): List<KeyValueItem> {
         val cursor = db.query(
             KeyValueEntry.TABLE_NAME,
@@ -39,11 +60,13 @@ class Control(context: Context) {
         cursor.close()
         return records
     }
+
     private fun delete(group: String, key: String): Boolean {
         val selection = "${KeyValueEntry.COLUMN_NAME_GROUP} = ? AND ${KeyValueEntry.COLUMN_NAME_KEY} = ?"
         val selectionArgs = arrayOf(group, key)
         return db.delete(KeyValueEntry.TABLE_NAME, selection, selectionArgs) > 0
     }
+
     private fun upsert(group: String, key: String, value: String): Boolean {
         val values = ContentValues().apply {
             put(KeyValueEntry.COLUMN_NAME_GROUP, group)
