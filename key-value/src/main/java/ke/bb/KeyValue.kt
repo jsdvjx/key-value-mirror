c50c8bf2bb6527c679fb8fa6329ce50292e9f364
Jin <admin@at.ci>
2022-10-22 23:35:04 +0800

    version

--- key-value/src/main/java/ke/bb/KeyValue.kt
+++ key-value/src/main/java/ke/bb/KeyValue.kt
@@ -7,6 +7,7 @@ abstract class BaseKeyValue(protected val control: Control)
 
 class KeyValue(private val context: Context) {
     private val control = Control(context)
+    @Suppress("UNCHECKED_CAST")
     fun <T> getInstance(clazz: Class<T>): T {
         val clazzName = "${clazz.name}Impl"
         return Class.forName(clazzName).getConstructor(Control::class.java).newInstance(control) as T
